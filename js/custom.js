51db1f75a897b808a0f7d9af5e545e0179aacf1c
flo preynat <flopreynat@users.noreply.github.com>
2023-09-13 13:58:39 +0200

    Update custom.js
    https gameaboutsquares

--- js/custom.js
+++ js/custom.js
@@ -7,7 +7,7 @@
 
 // Créez un tableau d'objets contenant les id et les URLs correspondantes
 const sites = [
-    { id: 'squaresBox', url: 'https://gameaboutsquares.com/' },
+    { id: 'squaresBox', url: 'http://gameaboutsquares.com/' },
     { id: 'dinosaurBox', url: 'https://www.trex-game.skipser.com/' },
     { id: 'eelslapBox', url: 'http://eelslap.com' },
     { id: 'shorttripBox', url: 'https://alexanderperrin.com.au/paper/shorttrip/' },
@@ -42,7 +42,7 @@ const sites = [
   
 
 squaresBox.addEventListener('click', () => {
-    contentIframe.src = 'https://gameaboutsquares.com/';
+    contentIframe.src = 'http://gameaboutsquares.com/';
     iframeContainer.style.display = 'block';
 });
 
