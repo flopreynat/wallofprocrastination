343fab807a8386a0e5acda7709b8db78c5a448e6
flo preynat <flopreynat@users.noreply.github.com>
2024-01-04 21:44:22 +0100

    Update custom.js
    dino3d

--- js/custom.js
+++ js/custom.js
@@ -34,7 +34,7 @@ const sites = [
     { id: 'sharkbeatBox', url: 'https://www.sharkbeat.com/' },
     { id: 'angrybirdsBox', url: 'https://sandstormdev.indie.af/' },
     { id: 'blackjackBox', url: 'https://www.html5blackjack.net/' },
-    { id: 'dino3dBox', url: 'https://dinosaurgameoffline.com/t-rex-run-3d' }
+    { id: 'dino3dBox', url: 'https://priler.github.io/dino3d/low.html' }
     // { id: 'xxxBox', url: '' },
 
   ];
